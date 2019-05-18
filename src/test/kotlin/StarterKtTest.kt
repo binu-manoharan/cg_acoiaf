ae7e20aa183006aac82dd986cf00561550ed45e6
binu manoharan <binu1988@gmail.com>
2019-05-18 04:24:57 +0100

    Init repo.

---
+++ src/test/kotlin/StarterKtTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class StarterKtTest {
+
+    @Test
+    fun main() {
+        println("hello world!")
+    }
+}
